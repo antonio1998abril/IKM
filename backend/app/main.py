a93e4f283c767fdd766fd7fada55e5a4300fb443
Antonio Rodriguez <anrodriguez@apexsystems.com>
2022-12-08 06:56:21 -0600

    footer graph

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,47 +1,222 @@
-
-
-import os
 import numpy as np
 import cv2
 from os import remove, rename, path
-from pdf2image import convert_from_path, convert_from_bytes
+from pdf2image import convert_from_path
 from PyPDF2 import PdfFileReader
 from typing import Union
 from fastapi import FastAPI, File, UploadFile
 from PIL import Image
 import platform
-import os.path
+from rapidfuzz import fuzz
+import math
 
-image_dir = os.path.join("./files/")
-testpdf =  "test.pdf"
-page0 =  "page0.jpg"
-CroppedImage =  "CroppedImage.jpg"
-Document011 =  "Document.011.png"
-js =  "js.png"
-getFinal=  "getFinal.png"
+if platform.system() == "Windows":     
+    image_dir = path.join("../files/")   
+elif platform.system() == "Linux":
+    image_dir = path.join("./files/")
 
+# Save pages in the same folder
+testpdf = path.join(image_dir, "test.pdf")
+page0 = path.join(image_dir,"page0.jpg" )
+CroppedImage = path.join(image_dir,"CroppedImage.jpg")
+Document011 = path.join(image_dir, "Document.011.png")
+js = path.join(image_dir, "js.png")
+getFinal = path.join(image_dir, "getFinal.png")
 
+#🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧 Footer Graphs 🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧🚧
+CroppedImageFooterCircle = path.join(image_dir,"CroppedImageFooterCircle.jpg")
+CroppedLevelUpRow1 = path.join(image_dir,"CroppedLevelUpRow1.jpg")
+CroppedLevelUpRow2 = path.join(image_dir,"CroppedLevelUpRow2.jpg")
+CroppedLevelUpRow1Blackest = path.join(image_dir,"CroppedLevelUpRow1Blackest.jpg")
+#pytorch
+AreaG = path.join(image_dir,"AreaG.jpg")
 
-testpdf = os.path.join(image_dir, testpdf)
-page0 = os.path.join(image_dir,page0 )
-CroppedImage = os.path.join(image_dir,CroppedImage)
-Document011 = os.path.join(image_dir, Document011)
-js = os.path.join(image_dir, js)
-getFinal = os.path.join(image_dir, getFinal)
-
-
-
-
-"""     remove("../files/test.pdf")
-        remove("../files/page0.jpg")
-        remove("../files/CroppedImage.jpg")
-        remove("../files/Document.011.png")
-        remove("../files/js.png")
-        remove("../files/getFinal.png") """
-
-
-
+#end points initializer
 app = FastAPI()
+
+# Round values
+def truncate(n, decimals = 0): 
+    multiplier = 10 ** decimals 
+    return int(n * multiplier) / multiplier #✅
+
+def round_up(n, decimals=0):
+    multiplier = 10 ** decimals
+    return math.ceil(n * multiplier) / multiplier
+###################################################################################
+#Circle get color
+def colorArea(area,image,color):
+    _, binary = cv2.threshold(area, 225, 255, cv2.THRESH_BINARY_INV)
+    contours, hierarchy = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # draw all contours
+    image = cv2.drawContours(image, contours, -1, (0, 255, 0), 2)
+    
+    for i in contours[1:]:
+        if truncate(cv2.contourArea(i), -4) > 1:#✅
+        #detect area ✅
+            #return {"color":color,"value":truncate(cv2.contourArea(i), -4)}#
+            return {"color":color,"value":cv2.contourArea(i)}#✅
+
+###################################################################################
+    #add tresHold to define image sections
+def CircleCV():
+    # Read the images
+    img = cv2.imread(CroppedImageFooterCircle)
+    
+    # Resizing the image
+    image = cv2.resize(img, (700, 600))
+    
+    # Convert Image to Image HSV
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    
+    #maskTofindColorsArea = ["green","red","yellow"]
+    #green
+    # Defining lower and upper bound HSV values
+    lowerGreen = np.array([50, 100, 100])
+    upperGreen = np.array([70, 255, 255])
+    # Defining mask for detecting color
+    maskGreen = cv2.inRange(hsv, lowerGreen, upperGreen)
+
+    #Red
+    lowerRed= np.array([170, 160, 70])
+    upperRed = np.array([180, 250, 250])
+    maskRed = cv2.inRange(hsv, lowerRed, upperRed)
+    #cv2.imshow("Red", maskRed)
+
+    #yellow
+    lowerYellow= np.array([18, 40, 90])
+    upperYellow = np.array([27, 255, 255])
+    maskYellow = cv2.inRange(hsv, lowerYellow, upperYellow)
+    #cv2.imshow("Yellow", maskYellow)
+
+    listArea = []
+
+    listArea.append(colorArea(maskRed,image,"Weak"))#✅
+    listArea.append(colorArea(maskYellow,image,"Proficient"))#✅
+    
+
+    flag = not np.any(maskGreen)
+    if flag:
+        listArea.append({"color":"Strong","value":0})
+    else:
+        listArea.append(colorArea(maskGreen,image,"Strong"))
+  
+  
+   
+    #✅
+    total = 0
+    for i in range(len(listArea)):
+        total = total +listArea[i]["value"]
+       
+
+
+    newListArea=[]
+    for i in range(len(listArea)):
+        
+
+        if(listArea[i]["color"] =="Proficient"):
+            result = {"color":"Proficient","value":truncate((listArea[i]["value"] / total) * 100) -1}
+            newListArea.append(result)
+          
+        if(listArea[i]["color"] =="Weak"):
+            result = {"color":"Weak","value":round_up((listArea[i]["value"] / total) * 100)}
+            newListArea.append(result)
+            
+    
+        if(listArea[i]["color"] =="Strong"):
+            result = {"color":"Strong","value":round_up((listArea[i]["value"] / total) * 100)}
+            newListArea.append(result)
+    
+ 
+    
+    flag= 0
+    for i in range(len(newListArea)):
+        if(newListArea[i]["color"] =="Strong" and newListArea[i]["value"] == 0.0):
+            flag = 1
+    for i in range(len(newListArea)):
+        if(newListArea[i]["color"] =="Proficient" and flag == 1):
+            newListArea[i]["value"] = newListArea[i]["value"] +1
+    
+       
+    print(newListArea)
+    return newListArea   
+    # Make python sleep for unlimited time
+   
+###################################################################################
+def deleteBlur(imageBlur):
+    #DELETE BLUR
+    img = cv2.imread(imageBlur)
+    Z = img.reshape((-1,3))
+
+    # convert to np.float32
+    Z = np.float32(Z)
+
+    # define criteria, number of clusters(K) and apply kmeans()
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+    K = 8
+    ret,label,center=cv2.kmeans(Z,K,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
+
+    # Now convert back into uint8, and make original image
+    center = np.uint8(center)
+    res = center[label.flatten()]
+    res2 = res.reshape((img.shape))    
+    cv2.imwrite(imageBlur,res2)
+
+###################################################################################
+def FooterRowLevelUp(image,context):
+    label =str("")
+    
+    img = cv2.imread(image)
+    # convert the image to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # apply thresholding to convert the grayscale image to a binary image
+    ret,thresh = cv2.threshold(gray,50,255,0)
+
+    # find the contours
+    contours,hierarchy = cv2.findContours(thresh, cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    if(context == "Work Speed/Accuracy"):
+        if len(contours) == 99:
+            label="Above Average"
+        elif len(contours) == 90:
+            label ="Average"
+        elif len(contours) == 89:
+            label = "Very Slow"
+        elif len(contours) == 93:
+            label = "Very Fast"
+        elif len(contours) == 101:
+            label = "Below Average"
+    elif(context == "Application Ability"):
+        cv2.imwrite(CroppedLevelUpRow1Blackest, thresh)
+        if len(contours) == 97:
+            label="Significant"
+        elif len(contours) == 96:
+            label ="Moderate"
+        elif len(contours) == 89:
+            label = "Negligible"
+        elif len(contours) >= 98:
+            label = "Extensive"
+        elif len(contours) == 90:
+            label = "Limited"
+    #✅Function Done
+    return {"label":label,"context":context}
+###################################################################################
+
+def nextword(target, source):
+    for i, w in enumerate(source):
+        if target == "ID:" or target == "Date:" or target == "Score:" or target == "Coverage:" or target == "subject.Percentile:":
+            if w == target:
+                return source[i+1]
+        elif target == "Subject:":
+            if w == target:
+                return source[i+1]+source[i+2]+source[i+3]+source[i+4]+source[i+5]+source[i+6]+source[i+7]
+        elif target == "for:":
+            if w == target:
+                return source[i+1]+" "+source[i+2]
+        elif target == "Client:":
+            if w == target:
+                return source[i+1]+" "+source[i+2]+" "+source[i+3]
+###################################################################################
+
 
 def functio7():
     javaOrder = []
@@ -54,6 +229,46 @@ def functio7():
     stringCleaned = " ".join(page_1_text.split())
 
     name.append(result[6] + " "+result[7])
+    #print(page_1_text.split(), sep='\n')
+
+    findInside =stringCleaned.split()
+    dataFile = [
+        "for:",
+        "ID:",
+        "Date:",
+        "Subject:",
+        "Client:",
+        "Score:",
+        "subject.Percentile:",
+        "Coverage:"
+    ]
+    dataMiner=[]
+    for i in range(len(dataFile)):
+        dataMiner.append(nextword(dataFile[i],findInside))
+
+    for i in range(len(dataMiner)):
+        if i == 3:
+            proximity= fuzz.ratio("MDC BASIC JAVA INTERVIEW 2",dataMiner[i])
+            if(proximity >= 60):
+                dataMiner[3]="MDC BASIC JAVA INTERVIEW 2" 
+
+    for i in range(len(dataMiner)):
+        if(i == 0):
+            dataMiner[i]={"label":"Assessment Result for","value":dataMiner[i]}
+        if(i == 1):
+            dataMiner[i]={"label":"ID","value":dataMiner[i]}
+        if(i == 2):
+            dataMiner[i]={"label":"Date","value":dataMiner[i]}
+        if(i == 3):
+            dataMiner[i]={"label":"Subject","value":dataMiner[i]}
+        if(i == 4):
+            dataMiner[i]={"label":"Client","value":dataMiner[i]}
+        if(i == 5):
+            dataMiner[i]={"label":"Score","value":dataMiner[i]}
+        if(i == 6):
+            dataMiner[i]={"label":"Percentile","value":dataMiner[i]}
+        if(i == 7):
+            dataMiner[i]={"label":"Subject Coverage","value":dataMiner[i]}   
 
     keywords = ["Agile Concepts",
                 "Design Patterns",
@@ -79,8 +294,8 @@ def functio7():
     javaOrder.sort(key=sortFn)
     javaOrder.reverse()
     dataSkills = []
-    # endGet text funtion
 
+    # Bar Graph
     image = cv2.imread(getFinal)
     deep_copy = image.copy()
 
@@ -96,12 +311,19 @@ def functio7():
         if cv2.contourArea(contour) > 1000:
             dataSkills.append(cv2.contourArea(contour))
             # print(cv2.contourArea(contour))
-    return {"scoreBar": dataSkills, "name": name, "concepts": javaOrder}
+   
+    listDataLevelUp = []
+    listDataLevelUp.append(FooterRowLevelUp(CroppedLevelUpRow1,"Work Speed/Accuracy"))
+    listDataLevelUp.append(FooterRowLevelUp(CroppedLevelUpRow2,"Application Ability"))
+    
+    #✅Return to Client
+    return {"scoreBar": dataSkills, "name": name, "concepts": javaOrder,"subjectCoverage":CircleCV(),"FooterLevel":listDataLevelUp,"dataMiner":dataMiner}
 
-
+###################################################################################
 def sortFn(dict):
+    #✅Function Done
     return dict['index']
-
+###################################################################################
 def functio6():
 
     image = cv2.imread(js)
@@ -130,9 +352,10 @@ def functio6():
         cv2.drawContours(image, [c], -1, (255, 255, 255), 2)
 
     cv2.imwrite(getFinal, image)
+    #✅Function Done
     return functio7()
 
-
+###################################################################################
 def function5():
     image = cv2.imread(Document011)
     msk = image.copy()
@@ -144,8 +367,9 @@ def function5():
     msk = cv2.erode(msk, np.ones((31, 31)))
     image[np.where(msk != 0)] = 255
     cv2.imwrite(js, image)
+    #✅Function Done
     return functio6()
-
+###################################################################################
 
 def function4():
     img = cv2.imread(CroppedImage)
@@ -157,21 +381,45 @@ def function4():
         thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     cv2.drawContours(img, contours, -1, (255, 255, 255), 2)
 
-    cv2.imwrite(Document011, img)
-    return function5()
+    cv2.imwrite(Document011, img)   
 
+    #DELETE BLUR ✅
+    #deleteBlur(CroppedImageFooterCircle)
+    deleteBlur(AreaG)
+    #✅Function Done
+    return function5()
+###################################################################################
 
 def function3():
     img = cv2.imread(page0)
     # Cropping an image
-
     #cropped_image = img[2050:3300, 850:2900]
     cropped_image = img[2050:3300, 1540:2900]
-
     # Save the cropped image
     cv2.imwrite(CroppedImage, cropped_image)
-    return function4()
 
+
+    # footer Graphs
+    cropped_image_footer = img[3580:4400, 2600:3500]    
+    cv2.imwrite(CroppedImageFooterCircle,cropped_image_footer)
+
+
+    #(arriba:abajo,derecha:izquierda)
+    #cropped_image_foteer = img[3500:4500, 1500:3000], bar cropped_image_foteerLevelUp1 = img[3500:4400, 1000:2500]
+    cropped_image_foteerLevelUp1 = img[3510:3720, 250:2050]
+    cv2.imwrite(CroppedLevelUpRow1, cropped_image_foteerLevelUp1)
+
+    cropped_image_foteerLevelUp2 = img[4120:4400, 250:2050]
+    cv2.imwrite(CroppedLevelUpRow2, cropped_image_foteerLevelUp2)
+    #✅Function Done
+
+    #py AreaG 
+    cropped_image_footerArea = img[3500:3600, 2500:3650]    
+    cv2.imwrite(AreaG,cropped_image_footerArea)
+
+
+    return function4()
+###################################################################################
 
 def function2():
     if platform.system() == "Windows":     
@@ -182,13 +430,11 @@ def function2():
         pages = convert_from_path(testpdf, 500)
         for i in range(len(pages)):
             pages[i].save('./files/page' + str(i) + '.jpg', 'JPEG')
-
-    """  pages = convert_from_path(testpdf, 500, poppler_path="../bin")
-    for i in range(len(pages)):
-        pages[i].save('../files/page' + str(i) + '.jpg', 'JPEG') """
+    #✅Function Done
     return function3()
 
-
+###################################################################################
+#END POINTS 👷👷👷👷👷👷
 def function1(contents):
     rename(contents, testpdf)
     return function2()
@@ -202,7 +448,6 @@ def read_root():
 @app.post("/submitFile")
 async def upload(file: UploadFile = File(...)):
     try:
-        listData = []
         contents = file.file.read()
         with open(file.filename, 'wb') as f:
             f.write(contents)
@@ -222,7 +467,10 @@ def delete_File():
         remove(Document011)
         remove(js)
         remove(getFinal)
+        remove(CroppedImageFooterCircle)
+        remove(AreaG)
+    
     except Exception:
-        return {"done": "There was an error when is trying to delete a file"}
+        return {"done": "There was an error when was trying to delete a file"}
     finally:
         return {"done": "Memory restored ready to upload another file"}
--- client/src/components/UploadFile.jsx
+++ client/src/components/UploadFile.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import axios from "axios";
 import { Modal, Form, Button } from "react-bootstrap";
-import { Bar } from "react-chartjs-2";
+import { Bar, PolarArea } from "react-chartjs-2";
 import {
   Chart as ChartJS,
   CategoryScale,
@@ -10,41 +10,79 @@ import {
   Title,
   Tooltip,
   Legend,
+  RadialLinearScale,
+  ArcElement,
 } from "chart.js";
 import Loading from "./Loading";
 
 function UploadFile() {
   const [uploadFile, setUploadFile] = useState(null);
+  /* 📝 Add labels to each bar */
+  /* Bar */
   const [labels, setLabels] = useState();
-  const [name, setName] = useState("");
   const [scoreBar, setScoreBar] = useState([]);
-
+  /* Circle */
+  const [scoreBarCircle, setScoreBarCircle] = useState([]);
+  /* Data Miner */
+  const [dataMiner,setDataMiner] = useState({
+    "Assessment Result for":"",
+    "ID":"", 
+    "Date":"",
+    "Subject":"",
+    "Client":"",
+    "Score":0,
+    "Percentile":0,
+    "Subject Coverage":0,
+    
+  });
+  const [name, setName] = useState("");
+  /* Footer Level */
+  const [footerLevel,setFooterLevel] = useState([
+    {label: '', context: 'Work Speed/Accuracy'},
+    {label: '', context: 'Application Ability'}
+  ]);
+  /* Set data for Bar graph */
   const [data, setData] = useState({
     labels: labels,
     datasets: [
       {
-        label: name,
+        label: "Subject Analysis",
         data: scoreBar,
         backgroundColor: "rgba(255, 99, 132, 0.5)",
       },
     ],
   });
+
+  /* Set data for  Circle graph */
+  const [dataCircle, setDataCircle] = useState({
+    labels: ["None"],
+    datasets: [
+      {
+        label: {"Subject Coverage":dataMiner["Subject Coverage"]},
+        data: scoreBarCircle,
+        backgroundColor: ["rgba(224, 0, 25, 0.8)"],
+      },
+    ],
+  });
+
   const inputFile = useRef(null);
   const updateTemplate = useRef(null);
   const [newCollaboratorModal, setNewCollaboratorModal] = useState(false);
   /* SPINNER LOADING */
   const [loading, setLoading] = useState(false);
-
+  /* 🔥🔥 Graph Configurations 🔥🔥 */
   ChartJS.register(
     CategoryScale,
     LinearScale,
     BarElement,
     Title,
     Tooltip,
-    Legend
+    Legend,
+    RadialLinearScale,
+    ArcElement
   );
 
-  const options = {
+  const optionsBar = {
     responsive: true,
     plugins: {
       legend: {
@@ -56,6 +94,24 @@ function UploadFile() {
       },
     },
   };
+
+  const optionsArea = {
+    type: "polarArea",
+    data: dataCircle,
+    options: {
+      responsive: true,
+      plugins: {
+        legend: {
+          position: "top",
+        },
+        title: {
+          display: true,
+          text: "Subject Coverage",
+        },
+      },
+    },
+  };
+  /* 🔥🔥 Graph Configurations 🔥🔥*/
 
   const onButtonClick = (e) => {
     e.preventDefault();
@@ -71,7 +127,9 @@ function UploadFile() {
       setLoading(true);
       setNewCollaboratorModal(true);
       await axios.post("/api/submitFile", formData).then((res) => {
+        console.log(res.data);
         setName(res.data.name);
+
         setScoreBar(
           res.data.scoreBar.map((item, index) => {
             let result = (item / 51923.5) * 100;
@@ -84,6 +142,23 @@ function UploadFile() {
             return item.value;
           })
         );
+
+        /* CIRCLE GRAPH */
+
+        let valuesCircle = [];
+        res.data.subjectCoverage.map((item, index) => {
+          return valuesCircle.push(item.value);
+        });
+        valuesCircle = valuesCircle.filter((item) => item !== 0);
+        setScoreBarCircle(valuesCircle);
+
+        /* Data Miner */
+
+     
+      
+        let object = res.data.dataMiner.reduce((obj, item) => (obj[item.label] = item.value, obj) ,{});
+        setDataMiner(object)
+        setFooterLevel(res.data.FooterLevel)
         setLoading(false);
         setUploadFile(null);
       });
@@ -101,17 +176,54 @@ function UploadFile() {
   };
 
   useEffect(() => {
+    /* Set default BarGraph */
     setData({
       labels,
       datasets: [
         {
-          label: name,
+          label: "Subject Analysis",
           data: scoreBar,
           backgroundColor: "rgba(255, 99, 132, 0.5)",
         },
       ],
     });
-  }, [uploadFile, name, labels, scoreBar]);
+
+    {
+      scoreBarCircle.length === 3
+        ? setDataCircle({
+            labels: ["Week", "Proficient", "Strong"],
+            datasets: [
+              {
+                label: `Subject Coverage, ${dataMiner["Subject Coverage"]}, This area`,
+                data: scoreBarCircle,
+
+                backgroundColor: [
+                  "rgba(224, 0, 25, 0.8)",
+                  "rgba(253, 156, 12, 0.8)",
+                  "rgba(2, 194, 68, 0.8)",
+                ],
+              },
+            ],
+          })
+        : setDataCircle({
+            labels: ["Week", "Proficient"],
+            datasets: [
+              {
+                label:`Subject Coverage: ${dataMiner["Subject Coverage"]},This area`,
+                data: scoreBarCircle,
+
+                backgroundColor: [
+                  "rgba(224, 0, 25, 0.8)",
+                  "rgba(253, 156, 12, 0.8)",
+                ],
+              },
+            ],
+          });
+    }
+  }, [uploadFile, name, labels, scoreBar, scoreBarCircle,dataMiner]);
+ 
+
+  console.log("data",footerLevel)
 
   return (
     <>
@@ -128,7 +240,22 @@ function UploadFile() {
                   <Modal.Title>USER SCORE: </Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                  <Bar options={options} data={data} />
+                  <h6>Client: {dataMiner.Client}, Date: {dataMiner.Date}, Subject: {dataMiner.Subject}</h6>
+                  <h6>ID: {dataMiner.ID}. </h6>
+                  <h6>Name: {name}, Score: {dataMiner.Score}.</h6>
+                  <h6>Percentile: {dataMiner.Percentile}.</h6>
+                  <Bar options={optionsBar} data={data}/>
+
+                
+                  <div>
+                  Work Speed/Accuracy: <h6 className="text-primary">{footerLevel[0].label}</h6>
+                  </div>
+                  <div>
+                  Application Ability : <h6 className="text-primary">{footerLevel[1].label}</h6>
+                  </div>
+
+                  <PolarArea options={optionsArea} data={dataCircle} />
+
                 </Modal.Body>
               </Form>
             </>
